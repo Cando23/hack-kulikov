2948d27b25829012579c8e2c7d0da1ace088fe21
Roman Polishchuk <rmn.polishchuk@gmail.com>
2022-02-28 20:58:30 +0300

    second iteration

--- epam.js
+++ epam.js
@@ -1,31 +1,49 @@
-function beforeMagic(){
-    const btns = document.querySelectorAll(".problem-action-btn");
-    for (let i = 0; i < btns.length; i++) {
-        btns[i].click();
+function setAnswsers(fieldSets) {
+  return new Promise((res) => {
+    for (let i = 0; i < fieldSets.length; i++) {
+      const answsers = fieldSets[i].querySelectorAll("span");
+      answsers[Math.floor(Math.random() * 4)].click();
     }
+    res();
+  });
 }
-
-function magic() {
-  let text = ''
+function submitAnswers() {
+  return new Promise((res) => {
+    const submits = document.querySelectorAll(".submit");
+    for (let i = 0; i < submits.length; i++) {
+      submits[i].click();
+    }
+    res();
+  });
+}
+function displayCorrectAnswers() {
+  const displayBtns = document.querySelectorAll(".problem-action-btn");
+  for (let i = 0; i < displayBtns.length; i++) {
+    displayBtns[i].click();
+  }
+}
+function copyCorrectAnswers(fieldSets) {
+  let text = "";
   const isCorrect = (label) => Boolean(label.querySelector(".correct"));
-  const fieldSets = document.getElementsByTagName("fieldSet");
   for (let i = 0; i < fieldSets.length; i++) {
     const labels = fieldSets[i].getElementsByTagName("label");
     const legend = fieldSets[i].getElementsByTagName("legend");
     text += legend[0].textContent + "\n";
     for (let j = 0; j < labels.length; j++) {
       if (isCorrect(labels[j])) {
-        text += '   ' + labels[j].childNodes[4].nodeValue.trim() + '\n';
+        text += "   " + labels[j].childNodes[4].nodeValue.trim() + "\n";
       }
     }
-    text += '\n\n';
+    text += "\n\n";
   }
-  return text;
+  console.log(text);
 }
-
-function hackEpam() {
-    beforeMagic();
-    setTimeout(() => console.log(magic()), 4000);
+async function hackEpam() {
+  const fieldSets = document.getElementsByTagName("fieldset");
+  await setAnswsers(fieldSets);
+  await submitAnswers();
+  setTimeout(displayCorrectAnswers, 2000);
+  setTimeout(copyCorrectAnswers, 5000, fieldSets);
 }
 
 hackEpam();
